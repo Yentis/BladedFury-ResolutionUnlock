16d3800c793f5c826b023ae19ed86468925a0345
Yentis <yentl178@gmail.com>
2024-11-16 12:47:51 +0100

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using ChopGhost;
+using HarmonyLib;
+using System.Reflection;
+
+namespace ResolutionUnlock
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = Logger;
+
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+            harmony.Patch(
+                original: typeof(OptionUI).GetMethod("SetupResolutionUI", BindingFlags.NonPublic | BindingFlags.Instance),
+                prefix: new HarmonyMethod(typeof(Patch).GetMethod(nameof(Patch.Prefix)))
+            );
+
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+        }
+    }
+
+    class Patch
+    {
+        private static readonly FieldInfo MaxWidthField = typeof(OptionUI).GetField("MAX_WIDTH", BindingFlags.NonPublic | BindingFlags.Static);
+        private static readonly FieldInfo MaxHeightField = typeof(OptionUI).GetField("MAX_HEIGHT", BindingFlags.NonPublic | BindingFlags.Static);
+
+        public static void Prefix()
+        {
+            MaxWidthField.SetValue(null, int.MaxValue);
+            MaxHeightField.SetValue(null, int.MaxValue);
+        }
+    }
+}
---
+++ obj/Debug/net35/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ResolutionUnlock
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "ResolutionUnlock";
+        public const string PLUGIN_NAME = "ResolutionUnlock";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
